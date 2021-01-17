bf2d64d647adee852c753831177da12bc2a59074
Taeyoon <naming0617@gmail.com>
2021-01-17 13:42:08 +0900

    init

---
+++ src/main/java/me/tom/onlinelecture/OnlinelectureApplication.java
@@ -0,0 +1,13 @@
+package me.tom.onlinelecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinelectureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OnlinelectureApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/tom/onlinelecture/OnlinelectureApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tom.onlinelecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinelectureApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
